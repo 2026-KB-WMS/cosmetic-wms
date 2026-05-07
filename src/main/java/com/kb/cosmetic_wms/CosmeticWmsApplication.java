42c37062e12230da83defcdb300793b727fdc68b
jinwoo <jjw6654@gmail.com>
2026-05-07 13:20:24 +0900

    chore: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/kb/cosmetic_wms/CosmeticWmsApplication.java
@@ -0,0 +1,13 @@
+package com.kb.cosmetic_wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/cosmetic_wms/CosmeticWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.cosmetic_wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
