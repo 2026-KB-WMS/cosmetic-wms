aad1520577cde02e3dd21fd0a43de4cea0a29e90
Jinwoo <112393728+jinwoojwa@users.noreply.github.com>
2026-05-21 14:45:48 +0900

    feat: Member 도메인 엔티티 및 검증 로직 구현 (#3)
    * feat(member): 회원가입 핵심 로직 구현
    
    [TDD Cycle]
    - RED: 회원가입 성공 테스트 추가
    - GREEN: 회원가입 핵심 로직 구현
    - REFACTOR: Member 클래스 생성자 접근 제어자 제한 및 Enum 매핑 전략(STRING) 적용
    
    [Description]
    - 회원 가입 전용 불변 객체인 MemberSignUpRequest 도입
    - 정적 팩토리 메서드 create을 사용한 Member 생성
    
    * feat(member): 이메일 검증 로직 구현
    
    [TDD Cycle]
    - RED: 올바르지 않은 이메일 형식 실패 테스트 작성
    - GREEN: validateEmail() 메서드를 통한 이메일 검증 로직 추가
    - REFACTOR: -
    
    [Description]
    -
    -
    
    * feat(member): 전화번호 검증 로직 구현
    
    [TDD Cycle]
    - RED: 올바르지 않은 전화번호 형식에 대한 실패 테스트 작성
    - GREEN: validatePhoneNumber() 메서드를 통한 전화번호 검증 로직 구현
    - REFACTOR: -
    
    [Description]
    -
    -
    
    * feat(member): 로그인 ID 관련 검증 로직 구현
    
    [TDD Cycle]
    - RED: 로그인 ID가 비어있거나 공백인 경우 예외를 던지는 테스트 작성
    - GREEN: ID가 null이거나 공백인 경우를 검증하는 validateLoginId() 메서드 구현
    - REFACTOR: -
    
    [Description]
    -
    -
    
    * feat(member): 로그인 ID 길이 검증 로직 추가
    
    [TDD Cycle]
    - RED: 로그인 ID 길이가 5자 ~ 50자 범위를 만족하는 지 여부를 테스트
    - GREEN: validateLoginId() 메서드에 ID 길이 관련 검증 로직 추가
    - REFACTOR: 상수값(ID 길이, 이메일, 전화번호 정규식) 분리 (MemberConstants.java)
    
    [Description]
    -
    -
    
    * feat(member): 비밀번호 예외 처리 로직 추가
    
    [TDD Cycle]
    - RED: 비밀번호의 공백, 길이, 규칙 관련 테스트 케이스 작성
    - GREEN: validatePassword() 메서드 구현
    - REFACTOR: -
    
    [Description]
    -
    -
    
    * chore(build.gradle): spring-boot-starter-validation 의존성 추가
    
    * feat(member): 가입 요청 DTO 유효성 검증 어노테이션 구현
    
    * refactor(member): 회원 도메인의 검증 메시지 상수화 및 DTO 검증 코드 추가
    
    - 회원 도메인의 validate 메서드에서 예외 처리 시 메시지를 상수화하여 관리
    - MemberSignUpRequest DTO에 검증 어노테이션 추가
    
    * refactor(member): Member.create 메서드의 DTO 의존 제거
    
    * test(member): MemberTestBuilder 추가 및 Member 테스트 리팩토링
    
    * refactor(member): Role enum 상수를 풀네임으로 변경
    
    * refactor(member): MemberConstants 클래스를 final 클래스로 변경

---
+++ src/main/java/com/kb/cosmetic_wms/domain/member/dto/MemberSignUpRequest.java
@@ -0,0 +1,43 @@
+package com.kb.cosmetic_wms.domain.member.dto;
+
+import com.kb.cosmetic_wms.domain.member.MemberConstants;
+import com.kb.cosmetic_wms.domain.member.enums.Role;
+import jakarta.validation.constraints.*;
+
+public record MemberSignUpRequest(
+        @NotBlank(message = MemberConstants.LOGIN_ID_REQUIRED_MESSAGE)
+        @Size(
+                min = MemberConstants.LOGIN_ID_MIN_LENGTH,
+                max = MemberConstants.LOGIN_ID_MAX_LENGTH,
+                message = MemberConstants.INVALID_LOGIN_ID_LENGTH_MESSAGE
+        )
+        String loginId,
+
+        @NotBlank(message = MemberConstants.PASSWORD_REQUIRED_MESSAGE)
+        @Size(
+                min = MemberConstants.PASSWORD_MIN_LENGTH,
+                max = MemberConstants.PASSWORD_MAX_LENGTH,
+                message = MemberConstants.INVALID_PASSWORD_LENGTH_MESSAGE
+        )
+        @Pattern(
+                regexp = MemberConstants.PASSWORD_REGEX,
+                message = MemberConstants.INVALID_PASSWORD_MESSAGE
+        )
+        String password,
+
+        @NotNull(message = MemberConstants.ROLE_REQUIRED_MESSAGE)
+        Role role,
+
+        @NotBlank(message = MemberConstants.MEMBER_NAME_REQUIRED_MESSAGE)
+        String memberName,
+
+        @Email(message = MemberConstants.INVALID_EMAIL_MESSAGE)
+        String email,
+
+        @Pattern(
+                regexp = MemberConstants.PHONE_NUMBER_REGEX,
+                message = MemberConstants.INVALID_PHONE_NUMBER_MESSAGE
+        )
+        String phoneNumber
+) {
+}
---
+++ src/main/java/com/kb/cosmetic_wms/domain/member/entity/Member.java
@@ -0,0 +1,98 @@
+package com.kb.cosmetic_wms.domain.member.entity;
+
+import com.kb.cosmetic_wms.domain.member.MemberConstants;
+import com.kb.cosmetic_wms.domain.member.enums.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loginId;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String memberName;
+    private String email;
+    private String phoneNumber;
+
+    private Member(String loginId, String password, Role role, String memberName, String email, String phoneNumber) {
+        this.loginId = loginId;
+        this.password = password;
+        this.role = role;
+        this.memberName = memberName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Member create(
+            String loginId,
+            String password,
+            Role role,
+            String memberName,
+            String email,
+            String phoneNumber
+    ) {
+        validateLoginId(loginId);
+        validatePassword(password);
+        validateEmail(email);
+        validatePhoneNumber(phoneNumber);
+
+        return new Member(
+                loginId,
+                password,
+                role,
+                memberName,
+                email,
+                phoneNumber
+        );
+    }
+
+    private static void validateEmail(String email) {
+        String emailRegex = MemberConstants.EMAIL_REGEX;
+        if (email == null || !Pattern.matches(emailRegex, email)) {
+            throw new IllegalArgumentException(MemberConstants.INVALID_EMAIL_MESSAGE);
+        }
+    }
+
+    private static void validatePhoneNumber(String phoneNumber) {
+        String phoneNumberRegex = MemberConstants.PHONE_NUMBER_REGEX;
+        if (phoneNumber == null || !Pattern.matches(phoneNumberRegex, phoneNumber)) {
+            throw new IllegalArgumentException(MemberConstants.INVALID_PHONE_NUMBER_MESSAGE);
+        }
+    }
+
+    private static void validateLoginId(String loginId) {
+        if (loginId == null || loginId.isBlank()) {
+            throw new IllegalArgumentException(MemberConstants.LOGIN_ID_REQUIRED_MESSAGE);
+        }
+        if (loginId.length() < MemberConstants.LOGIN_ID_MIN_LENGTH ||
+                loginId.length() > MemberConstants.LOGIN_ID_MAX_LENGTH) {
+            throw new IllegalArgumentException(MemberConstants.INVALID_LOGIN_ID_LENGTH_MESSAGE);
+        }
+    }
+
+    private static void validatePassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException(MemberConstants.PASSWORD_REQUIRED_MESSAGE);
+        }
+        if (password.length() < MemberConstants.PASSWORD_MIN_LENGTH ||
+                password.length() > MemberConstants.PASSWORD_MAX_LENGTH) {
+            throw new IllegalArgumentException(MemberConstants.INVALID_PASSWORD_LENGTH_MESSAGE);
+        }
+        if (!Pattern.matches(MemberConstants.PASSWORD_REGEX, password)) {
+            throw new IllegalArgumentException(MemberConstants.INVALID_PASSWORD_MESSAGE);
+        }
+    }
+}
