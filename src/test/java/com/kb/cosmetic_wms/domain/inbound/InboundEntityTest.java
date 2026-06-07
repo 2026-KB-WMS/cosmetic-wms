425d91fe0868f5dce829423ffea1513c2aeb58ce
jinwoo <jjw6654@gmail.com>
2026-06-07 12:39:57 +0900

    fix: 입고 생성 날짜 오류로 인한 테스트 실패 수정

--- src/test/java/com/kb/cosmetic_wms/domain/inbound/InboundEntityTest.java
+++ src/test/java/com/kb/cosmetic_wms/domain/inbound/InboundEntityTest.java
@@ -19,7 +19,7 @@ public class InboundEntityTest {
     @Test
     void 입고_엔티티는_필수_값이_모두_존재하면_SCHEDULED_상태로_성공적으로_생성된다() {
         // given
-        LocalDateTime inboundDate = LocalDateTime.of(2030, 6, 1, 0, 0);
+        LocalDateTime inboundDate = LocalDateTime.now().plusYears(1);
         Warehouse warehouse = new WarehouseTestBuilder().build();
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
 
@@ -33,7 +33,7 @@ public class InboundEntityTest {
     @Test
     void 입고_예정일이_현재_날짜보다_과거이면_예외를_던진다() {
         // given
-        LocalDateTime pastInboundDate = LocalDateTime.of(2025, 5, 31, 23, 59);
+        LocalDateTime pastInboundDate = LocalDateTime.now().minusYears(1);
         Warehouse warehouse = new WarehouseTestBuilder().build();
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
 
@@ -48,7 +48,7 @@ public class InboundEntityTest {
     @Test
     void 입고_생성_시_창고_객체가_누락되면_예외를_던진다() {
         // given
-        LocalDateTime inboundDate = LocalDateTime.of(2040, 6, 1, 0, 0);
+        LocalDateTime inboundDate = LocalDateTime.now().plusYears(1);
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
 
         // when & then
@@ -62,7 +62,7 @@ public class InboundEntityTest {
     @Test
     void 입고_예정_상태에서는_취소가_가능하다() {
         // given
-        LocalDateTime inboundDate = LocalDateTime.of(2026, 6, 5, 0, 0);
+        LocalDateTime inboundDate = LocalDateTime.now().plusYears(1);
         Warehouse warehouse = new WarehouseTestBuilder().build();
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
         Inbound inbound = Inbound.create(inboundDate, warehouse, partner);
@@ -77,7 +77,7 @@ public class InboundEntityTest {
     @Test
     void 이미_작업이_진행_중인_입고_건은_취소_시_예외를_던진다() {
         // given
-        LocalDateTime inboundDate = LocalDateTime.of(2026, 6, 5, 0, 0);
+        LocalDateTime inboundDate = LocalDateTime.now().plusYears(1);
         Warehouse warehouse = new WarehouseTestBuilder().build();
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
         Inbound inbound = Inbound.create(inboundDate, warehouse, partner);
@@ -94,7 +94,7 @@ public class InboundEntityTest {
     @Test
     void 입고_완료_처리는_입고_진행_상태에서만_가능해야_한다() {
         // given
-        LocalDateTime inboundDate = LocalDateTime.of(2026, 6, 5, 0, 0);
+        LocalDateTime inboundDate = LocalDateTime.now().plusYears(1);
         Warehouse warehouse = new WarehouseTestBuilder().build();
         Partner partner = Partner.create("본사", PartnerType.HEADQUARTER, "123-45-67890");
         Inbound inbound = Inbound.create(inboundDate, warehouse, partner);
